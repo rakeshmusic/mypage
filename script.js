b8e46ac043d6674d415a05e81addd3c87fdb0afb
Rakesh Kumar <rakeshmusic@gmail.com>
2022-05-01 17:27:38 +0530

    main functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+function load(){
+  const head = document.querySelector("#heading");
+  head.innerText = "hello1";
+}
+
+document.addEventListener('DOMContentLoaded', load);
+
+function closure(){
+  let x=0;
+  function increment(){
+    x++;
+    console.log(x);
+  }
+  function decrement(){
+    x--;
+    console.log(x);
+  }
+  function Init(){
+    x=0;
+  }
+  return {
+    increment,
+    decrement
+  }
+}
+
+let y = closure();
+y.increment();
+y.increment();
+y.increment();
+y.decrement();
+
